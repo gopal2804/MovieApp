f3dc95faabf870fcb81236f7702d2ae6ced60d0b
gopal_28 <gopalgupta2804@gmail.com>
2020-04-27 23:36:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+const request=require('request');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+app.use(express.static('public'));
+
+app.get('/', (req,res)=>{
+    res.render('search');
+});
+
+app.get("/results",(req,res)=>{
+
+    let query=req.query.search;
+    request('https://api.themoviedb.org/3/search/movie?api_key=d139e8df84838442f52ad55706176199&query='+query, (error,response,body)=>{
+        if(error){
+            console.log(error);
+        }
+        let data=JSON.parse(body);
+        res.render('movies',{data:data, searchQuery:query.toUpperCase()});
+    });
+});
+
+app.listen(process.env.PORT,process.env.ID,()=>{
+    console.log("server started");
+});
